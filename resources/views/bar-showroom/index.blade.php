17a7c43075285ffc4b791d2f9718e60962ec7f85
Roland Vrignon <roland.vrignon@gmail.com>
2017-10-19 19:43:36 +0200

    affichage des différents bars dans la page bars

--- resources/views/bar-showroom/index.blade.php
+++ resources/views/bar-showroom/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.bar')
 @section('content')
 
-    ygjhgkjhk
+
+    <h1 class="text-center">Voici notre séléction</h1>
+
+
+    <div class="container">
+        <div class="row">
+                    @forelse($bars as $bar)
+                    <div class="col-sm-4 portfolio-item">
+                        <h3>{{$bar->title}}</h3>
+                        {{$bar->description}}
+                    </div>
+                    @empty
+                        <li>Aucune tâche</li>
+                    @endforelse
+        </div>
+    </div>
+
+
 
 @endsection
