9f5e510a9ad548407a33ec2f83af2e5bc8a8d6c8
Roland Vrignon <roland.vrignon@gmail.com>
2017-10-30 16:22:50 +0100

    debug routes 2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -35,5 +38,12 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect('/home');
     }
 }
--- resources/views/layouts/bar.blade.php
+++ resources/views/layouts/bar.blade.php
@@ -45,11 +45,14 @@
                 </li>
 
             </ul>
+
+
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
                 <li><a href="{{ route('login') }}">Login</a></li>
                 <li><a href="{{ route('register') }}">Register</a></li>
+
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -58,7 +61,7 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href="{{route('bars.create')}}">
+                                <a href="{{route('bars.index')}}">
                                     Mes Bars
                                 </a>
                             </li>
